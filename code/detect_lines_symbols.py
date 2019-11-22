3bd5093db3928bfe036b4d1ca567aec792ad9819
Добровольский Александр Вячеславович СИТиС <dobrovolskiy_av.sit@mosgortrans.ru>
2019-11-22 16:25:56 +0300

    write_to_json func

--- code/detect_lines_symbols.py
+++ code/detect_lines_symbols.py
@@ -19,3 +19,17 @@ def show_image(image):
 
 def save_image(image, filename):
     cv2.imwrite(filename, image)
+
+def write_to_json(stats, filename):
+    data = []
+    for ind, stat in enumerate(stats):
+        data.append({
+            'x': stat[0],
+            'y': stat[1],
+            'w': stat[2],
+            'h': stat[3],
+            'A': stat[4]
+        })
+    with open(filename, 'wb') as outfile:
+    # store the data as binary data stream
+        pickle.dump(data, outfile)
