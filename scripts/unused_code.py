2ab6015bc3e20b4045a12935786017ae885b63c4
br-eina <bruenor11@gmail.com>
2020-04-24 17:22:57 +0300

    comment unused_code

--- scripts/unused_code.py
+++ scripts/unused_code.py
@@ -78,80 +78,80 @@
 
 
 
-# Сохранение блоков, не лежащих в таблице:
-def save_blocks_not_in_table(text_blocks, image, image_name):
-    blocks_not_in_table = []
-    for row in text_blocks:
-        for block in row:
-            if block['in_table'] == False:
-                blocks_not_in_table.append(block)
-    save_image_blocks_not_in_table(image, blocks_not_in_table, image_name)
-    with open("data_not_in_table/blocks_not_in_table_{0}.data".format(image_name), 'wb') as f:
-        pickle.dump(blocks_not_in_table, f)
+# # Сохранение блоков, не лежащих в таблице:
+# def save_blocks_not_in_table(text_blocks, image, image_name):
+#     blocks_not_in_table = []
+#     for row in text_blocks:
+#         for block in row:
+#             if block['in_table'] == False:
+#                 blocks_not_in_table.append(block)
+#     save_image_blocks_not_in_table(image, blocks_not_in_table, image_name)
+#     with open("data_not_in_table/blocks_not_in_table_{0}.data".format(image_name), 'wb') as f:
+#         pickle.dump(blocks_not_in_table, f)
 
-# Сохранение символов, не лежащих в таблице:
-def save_symbols_not_in_table(text_elements, tables, image, image_name):
-    symbols_not_in_table = []
-    for table in tables:
-        for ind, symbol in enumerate(text_elements):
-            if elem_in_table(symbol, table):
-                text_elements[ind]['in_table'] = True
-    for symbol in text_elements:
-        if symbol['in_table'] == False:
-            symbols_not_in_table.append(symbol)
-    save_image_symbols_not_in_table(image, symbols_not_in_table, image_name)
-    with open("data_not_in_table/symbols_not_in_table_{0}.data".format(image_name), 'wb') as outfile:
-        pickle.dump(symbols_not_in_table, outfile)
+# # Сохранение символов, не лежащих в таблице:
+# def save_symbols_not_in_table(text_elements, tables, image, image_name):
+#     symbols_not_in_table = []
+#     for table in tables:
+#         for ind, symbol in enumerate(text_elements):
+#             if elem_in_table(symbol, table):
+#                 text_elements[ind]['in_table'] = True
+#     for symbol in text_elements:
+#         if symbol['in_table'] == False:
+#             symbols_not_in_table.append(symbol)
+#     save_image_symbols_not_in_table(image, symbols_not_in_table, image_name)
+#     with open("data_not_in_table/symbols_not_in_table_{0}.data".format(image_name), 'wb') as outfile:
+#         pickle.dump(symbols_not_in_table, outfile)
 
-def save_text_blocks(text_blocks_rows, image_name):
-    with open("inv_processing/data/data_text_blocks_tables_{0}.data".format(image_name), 'wb') as outfile:
-        pickle.dump(text_blocks_rows, outfile)
+# def save_text_blocks(text_blocks_rows, image_name):
+#     with open("inv_processing/data/data_text_blocks_tables_{0}.data".format(image_name), 'wb') as outfile:
+#         pickle.dump(text_blocks_rows, outfile)
 
-def save_image_blocks_not_in_table(image, blocks_not_in_table, image_name):
-    image_to_show = image.copy()
-    for element in blocks_not_in_table:
-        p_1 = (element['x'], element['y'])
-        p_2 = (element['x'] + element['w'], element['y'] + element['h'])
-        cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 2)
-    utils.save_image(image_to_show, "results/{0}/{0}_blocks_not_in_table.jpg".format(image_name))
+# def save_image_blocks_not_in_table(image, blocks_not_in_table, image_name):
+#     image_to_show = image.copy()
+#     for element in blocks_not_in_table:
+#         p_1 = (element['x'], element['y'])
+#         p_2 = (element['x'] + element['w'], element['y'] + element['h'])
+#         cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 2)
+#     utils.save_image(image_to_show, "results/{0}/{0}_blocks_not_in_table.jpg".format(image_name))
 
-def save_image_symbols_not_in_table(image, symbols_not_in_table, image_name):
-    image_to_show = image.copy()
-    for element in symbols_not_in_table:
-        p_1 = (element['x'], element['y'])
-        p_2 = (element['x'] + element['w'], element['y'] + element['h'])
-        cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 2)
-    utils.save_image(image_to_show, "results/{0}/{0}_symbols_not_in_table.jpg".format(image_name))
-
-
-def save_tables_lines(image, lines_elem, image_name):
-    image_to_show = image.copy()
-    for element in lines_elem:
-        p_1 = (element['x'], element['y'])
-        p_2 = (element['x'] + element['w'], element['y'] + element['h'])
-        cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 3)
-    utils.save_image(image_to_show, "results/{}_tables.jpg".format(image_name))
-    utils.save_image(image_to_show, "results/{0}/{0}_tables.jpg".format(image_name))
+# def save_image_symbols_not_in_table(image, symbols_not_in_table, image_name):
+#     image_to_show = image.copy()
+#     for element in symbols_not_in_table:
+#         p_1 = (element['x'], element['y'])
+#         p_2 = (element['x'] + element['w'], element['y'] + element['h'])
+#         cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 2)
+#     utils.save_image(image_to_show, "results/{0}/{0}_symbols_not_in_table.jpg".format(image_name))
 
 
-def check_number_blocks_in_table(tables, text_blocks_rows):
-    # Add parameter to text block
-    # If in table: True
+# def save_tables_lines(image, lines_elem, image_name):
+#     image_to_show = image.copy()
+#     for element in lines_elem:
+#         p_1 = (element['x'], element['y'])
+#         p_2 = (element['x'] + element['w'], element['y'] + element['h'])
+#         cv2.rectangle(image_to_show, p_1, p_2, (0, 0, 255), 3)
+#     utils.save_image(image_to_show, "results/{}_tables.jpg".format(image_name))
+#     utils.save_image(image_to_show, "results/{0}/{0}_tables.jpg".format(image_name))
 
-    for ind_row, row in enumerate(text_blocks_rows):
-        for ind_block, block in enumerate(row):
-            for table in tables:
-                if (
-                    (block['x'] >= table['x']) and (block['x'] + block['w'] <= table['x'] + table['w']) and
-                    (block['y'] >= table['y']) and (block['y'] + block['h'] <= table['y'] + table['h'])
-                ):
-                    text_blocks_rows[ind_row][ind_block]['in_table'] = True
 
-def find_tables_without_lines(line_elements_copy, hor_lines, vert_lines,
-                              table_elements_with_lines):
-    for table in line_elements_copy:
-        if table_by_lines(table, hor_lines, vert_lines) == 1:
-            table_elements_with_lines.append(table)
+# def check_number_blocks_in_table(tables, text_blocks_rows):
+#     # Add parameter to text block
+#     # If in table: True
+
+#     for ind_row, row in enumerate(text_blocks_rows):
+#         for ind_block, block in enumerate(row):
+#             for table in tables:
+#                 if (
+#                     (block['x'] >= table['x']) and (block['x'] + block['w'] <= table['x'] + table['w']) and
+#                     (block['y'] >= table['y']) and (block['y'] + block['h'] <= table['y'] + table['h'])
+#                 ):
+#                     text_blocks_rows[ind_row][ind_block]['in_table'] = True
+
+# def find_tables_without_lines(line_elements_copy, hor_lines, vert_lines,
+#                               table_elements_with_lines):
+#     for table in line_elements_copy:
+#         if table_by_lines(table, hor_lines, vert_lines) == 1:
+#             table_elements_with_lines.append(table)
 
 
 
