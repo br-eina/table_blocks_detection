aac46a1ff0c96ed2e5be677849be44b06fd7561b
br-eina <bruenor11@gmail.com>
2020-05-09 12:20:43 +0300

    Add args to avoid hardcoding
    Specify element type
    Specify the field around which the bbox is constructed
    Specify the folder ender

--- scripts/draw_predictions.py
+++ scripts/draw_predictions.py
@@ -3,9 +3,21 @@ import cv2
 import pandas as pd
 from inv_processing import utils
 
-def main():
-    """Read dataset with 'prediction' field and draw predictions on the images"""
-    path_df = 'pred_symbols.csv'
+def main(element_type, prediction=True, folder_ender=""):
+    """Read dataset with 'prediction' field and draw predictions on the images.
+
+        Args:
+            element_type (str): has to be either 'blocks' or 'symbols'.
+            prediction (bool, optional): if True - draw bboxes for predictions, otherwise (False) for labels.
+                Defaults to True.
+            folder_ender (str, optional): ender of the folder (labels{folder_ender}).
+                Defaults to empty string.
+
+        Returns:
+            None
+
+    """
+    path_df = f'pred_{element_type}.csv'
     df = pd.read_csv(path_df)
 
     test_filenames = df['filename'].unique()
@@ -15,14 +27,27 @@ def main():
         row = df_inv.to_dict('records')
 
         image_path = f'docs/test/{image_name}.jpg'
-        predict_folder = 'labels/symbols'
+        if prediction:
+            label_field = 'prediction'
+            if element_type == 'blocks':
+                folder = f'predictions{folder_ender}'
+            else:
+                folder = f'predictions{folder_ender}/symbols'
+        else:
+            label_field = 'label'
+            if element_type == 'blocks':
+                folder = f'labels{folder_ender}'
+            else:
+                folder = f'labels{folder_ender}/symbols'
+
         image = cv2.imread(image_path)
 
-        utils.create_folders(predict_folder)
-        utils.save_image(image, path=f'{predict_folder}/{image_name}.jpg')
+        utils.create_folders(folder)
+        utils.save_image(image, path=f'{folder}/{image_name}.jpg')
 
-        utils.bounding_boxes(image, image_name, label_field='label', method='row', row=row,
-                             path=f'{predict_folder}/{image_name}_pred.jpg')
+        utils.bounding_boxes(image, image_name, label_field=label_field, method='row', row=row,
+                             path=f'{folder}/{image_name}_pred.jpg')
 
 if __name__ == "__main__":
-    main()
+    _ELEMENT_TYPE = 'blocks'
+    main(_ELEMENT_TYPE)
