e49ad6e5e169ee537f40eb13527408faf9ef4ca9
br-eina <bruenor11@gmail.com>
2020-06-02 11:14:45 +0300

    Dash tutorial

---
+++ dash/tutorial/three_cols.py
@@ -0,0 +1,78 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+#Define Dash App
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app=dash.Dash(external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Div([
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar'}
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization',
+                    'height':500
+                    }
+                }
+            ),
+        ],
+        style={'width': '33%', 'display': 'inline-block'}
+        ),
+
+    html.Div([
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': 'Montreal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+                ],
+            value=['MTL', 'SF'],
+            multi=True
+        ),
+
+        dcc.Graph(
+            id='example-graph3',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar'}
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization',
+                    'height':250
+                    }
+                }
+            )
+        ],
+        style={'width': '33%', 'display': 'inline-block'}
+        ),
+
+    html.Div([
+        dcc.Graph(
+            id='example-graph4',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar'}
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization',
+                    'height':500
+                    }
+                }
+            )
+        ],
+        style={'width': '33%', 'display': 'inline-block', 'float': 'right'},
+        )
+    ]
+)
+
+
+if __name__ == '__main__':
+    app.run_server()
